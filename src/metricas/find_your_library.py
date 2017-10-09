ee4be56a0b798f5463adb5e1f10ca68205b9469a
Douglas Cerqueira <douglasdacruzz@gmail.com>
2017-10-09 17:24:23 +0000

    Escrito método library expertise

--- src/metricas/find_your_library.py
+++ src/metricas/find_your_library.py
@@ -1,7 +1,22 @@
 # Implementation of metrics from "Find Your Library Experts"
 
-def library_expertise():
+# Define o conjunto de simbolos usado pelo desenvolvedor
+def symbols_usage():
     return None
+
+# Conjunto de simbolos usados por todos os desenvolvedores    
+def provided_symbols():
+    return None
+
+# Relação entre o uso de um método pelo desenvolvedor e todo o uso dentro do projeto
+def library_expertise(provided_symbol, usage_symbol):
+    developers = {}
+    for key, value in usage_symbol.iteritems():
+        expertise = {key: usage_symbol[key] / provided_symbol.get(key, 0) for key in usage_symbol.keys()}
+        developers[key] = expertise
+    
+    return developers
+
 
 def expertise_distance():
     return None
